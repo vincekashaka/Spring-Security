40b73f46ddff8424ac6e94d8f641c371c4a3fd2a
Vincent Kashaka <vincekashaka@gmail.com>
2022-09-08 14:24:08 +0200

    Added password encoder

--- src/main/java/vince/com/security/security/ApplicationSecurityConfig.java
+++ src/main/java/vince/com/security/security/ApplicationSecurityConfig.java
@@ -1,28 +1,50 @@
 package vince.com.security.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.SecurityBuilder;
-import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
+
 public class ApplicationSecurityConfig {
 
+private final PasswordEncoder passwordEncoder;
+
+@Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
 
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                .antMatchers("/", "index", "/css/*", "/js/*")
+                .permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
                 .httpBasic();
         return http.build();
+
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails vinceUser = User.builder()
+                .username("vince")
+                .password(passwordEncoder.encode("password"))
+                .roles("STUDENT") // ROLE_STUDENT
+                .build();
+        return new InMemoryUserDetailsManager(vinceUser);
     }
 
 
