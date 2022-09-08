c553c2b47e1716e5e0b5f65f6e07d432ea657850
Vincent Kashaka <vincekashaka@gmail.com>
2022-09-08 19:00:12 +0200

    added guava dependency

--- src/main/java/vince/com/security/security/ApplicationSecurityConfig.java
+++ src/main/java/vince/com/security/security/ApplicationSecurityConfig.java
@@ -3,16 +3,23 @@ package vince.com.security.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import vince.com.security.student.Student;
+
+import static vince.com.security.security.ApplicationUserPermission.*;
+import static vince.com.security.security.ApplicationUserRole.*;
 
 @Configuration
-
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ApplicationSecurityConfig {
 
 private final PasswordEncoder passwordEncoder;
@@ -26,9 +33,14 @@ private final PasswordEncoder passwordEncoder;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/", "index", "/css/*", "/js/*")
-                .permitAll()
+                .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
+                .antMatchers("/api/**").hasRole(STUDENT.name())
+//                .antMatchers( HttpMethod.DELETE, "/management/api/**").hasAnyAuthority(COURSE_WRITE.getPermission())
+//                .antMatchers( HttpMethod.POST, "/management/api/**").hasAnyAuthority(COURSE_WRITE.getPermission())
+//                .antMatchers( HttpMethod.PUT, "/management/api/**").hasAnyAuthority(COURSE_WRITE.getPermission())
+//                .antMatchers(HttpMethod.GET,"/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -42,9 +54,30 @@ private final PasswordEncoder passwordEncoder;
         UserDetails vinceUser = User.builder()
                 .username("vince")
                 .password(passwordEncoder.encode("password"))
-                .roles("STUDENT") // ROLE_STUDENT
+//                .roles(STUDENT.name()) // ROLE_STUDENT
+                .authorities(STUDENT.getGrantedAuthority())
                 .build();
-        return new InMemoryUserDetailsManager(vinceUser);
+
+        //Create Admin role
+        UserDetails lisaUser = User.builder()
+                .username("lisa")
+                .password(passwordEncoder.encode("password123"))
+//                .roles(ADMIN.name()) // Admin_Role
+                .authorities(ADMIN.getGrantedAuthority())
+                .build();
+
+        UserDetails tomUser = User.builder()
+                .username("tom")
+                .password(passwordEncoder.encode("password123"))
+//                .roles(ADMINTRAINEE.name()) // ROLE_ADMINTRAINEE
+                .authorities(ADMINTRAINEE.getGrantedAuthority())
+                .build();
+
+        return new InMemoryUserDetailsManager(
+                vinceUser,
+                lisaUser,
+                tomUser
+        );
     }
 
 
